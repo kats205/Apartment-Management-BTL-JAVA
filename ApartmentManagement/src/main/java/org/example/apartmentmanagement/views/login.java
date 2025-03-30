58b049080b5dfb30c27688e9b1f02f1e8486f32d
kats205 <npbaokhanhpro@gmail.com>
2025-03-31 00:03:03 +0700

    them login

--- ApartmentManagement/src/main/java/org/example/apartmentmanagement/views/login.java
+++ ApartmentManagement/src/main/java/org/example/apartmentmanagement/views/login.java
@@ -1,6 +1,5 @@
 package org.example.apartmentmanagement.views;
 
-import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -8,14 +7,12 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class login extends Application {
-    public login() {
-    }
 
-    public void start(Stage primaryStage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(this.getClass().getResource("/org/example/apartmentmanagement/fxml/login-view.fxml"));
-        Scene scene = new Scene((Parent)fxmlLoader.load());
-        primaryStage.setTitle("Apartment Management - Login");
-        primaryStage.setScene(scene);
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/org/example/apartmentmanagement/fxml/login-view.fxml"));
+        primaryStage.setTitle("Apartment Application");
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
@@ -23,3 +20,4 @@ public class login extends Application {
         launch(args);
     }
 }
+
