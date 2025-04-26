9c3d5da9461a1bd934540754399ea266bf1ee8ef
gdev-binh <duybinh7700@gmail.com>
2025-04-26 15:27:37 +0700

    them cac chuc nang moi

---
+++ ApartmentManagement/src/main/java/com/utc2/apartmentmanagement/Payment/vnpay/VnpayHashUtil.java
@@ -0,0 +1,23 @@
+package org.example.apartmentmanagement.Payment.vnpay;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.nio.charset.StandardCharsets;
+
+public class VnpayHashUtil {
+    public static String hmacSHA512(String key, String data) {
+        try {
+            Mac hmac = Mac.getInstance("HmacSHA512");
+            SecretKeySpec secretKey = new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA512");
+            hmac.init(secretKey);
+            byte[] bytes = hmac.doFinal(data.getBytes(StandardCharsets.UTF_8));
+            StringBuilder hash = new StringBuilder();
+            for (byte b : bytes) {
+                hash.append(String.format("%02x", b));
+            }
+            return hash.toString();
+        } catch (Exception ex) {
+            throw new RuntimeException("Lỗi mã hóa HMAC SHA512", ex);
+        }
+    }
+}
