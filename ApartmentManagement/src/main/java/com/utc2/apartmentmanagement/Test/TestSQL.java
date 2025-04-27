6ade2a77700a9258d27853be6b464a9dac1066aa
kats205 <npbaokhanhpro@gmail.com>
2025-04-27 10:44:14 +0700

    Delete ApartmentManagement directory

--- ApartmentManagement/src/main/java/com/utc2/apartmentmanagement/Test/TestSQL.java
+++
@@ -1,8 +0,0 @@
-package com.utc2.apartmentmanagement.Test;
-
-import java.io.IOException;
-import java.sql.SQLException;
-
-public class TestSQL {
-
-}
--- ApartmentManagement/src/main/java/com/utc2/apartmentmanagement/Utils/ScannerUtils.java
+++
@@ -1,4 +0,0 @@
-package com.utc2.apartmentmanagement.Utils;
-
-public class ScannerUtils {
-}
